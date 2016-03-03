0055069c617918dd0aa8debf4f6bae1d62e341c7
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2016-03-03 23:13:40 +0530

    login complete

--- app/components/UserLogin.jsx
+++ app/components/UserLogin.jsx
@@ -19,16 +19,23 @@ class UserLogin extends React.Component{
 
         Request.post("http://localhost:3000/tokens/verify")
         .send({user:user})
-        .end(function(res,err){
+        .end((err,res) => {
           console.log(res)
+          var response = JSON.parse(res.text)
+          console.log(response)
+          if(res.status == 200)
+          	localStorage.user_token = response.token
         })
 	}
 	render(){
 		return(
-			<div className='login'>
-				<input ref="username" type="text" placeholder="username" name="username" />
-				<input ref="password" type="text" placeholder="password" name="password" />
-				<input type="submit" value="Login" className="submit-long" onClick={this.handleLogin.bind(this)}/>
+			<div>
+				<div className='login-bg'></div>
+				<div className='login'>
+					<input ref="username" type="text" placeholder="username" />
+					<input ref="password" type="password" placeholder="password" />
+					<input type="submit" value="Login" className="submit-long" onClick={this.handleLogin.bind(this)}/>
+				</div>
 			</div>
 			)
 	}
