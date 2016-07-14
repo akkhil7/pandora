e06be07d8968f66a0cf08f1ff973937189baedcd
Akhil Ramesh <akkhil@outlook.com>
2016-07-15 02:13:06 +0530

    code desc page added + styling

--- app/components/CodeDesc.jsx
+++ app/components/CodeDesc.jsx
@@ -2,36 +2,79 @@ import React from 'react';
 import Router from 'react-router';
 import UserNavbar from './UserNavbar.jsx';
 import API from './API.js';
+import StarRating from 'react-star-rating';
 
 class CodeDesc extends React.Component{
-	constructor(){
-		super()
-	}
-	componentDidMount() {
-	    var id  = this.props.params.id
-	    var url = API.url('codes',id)
-	    var success = function(res) {
-        	console.log('success')
-        }
-    	var failure = function(res) {
-      	console.log('failure')
-        }
-	    API.get(url,success,failure)  
-	}
+  constructor(){
+    super()
+    this.state = {
+      result: {}
+    }
+  }
+  componentDidMount() {
+    var id  = this.props.params.id
+    var url = API.url('codes',id)
+    var success = (res) => {
+      console.log('success')
+      console.log(res)
+      this.setState({result: res})
+    }
+    var failure = function(res) {
+      console.log('failure')
+    }
+    API.get(url,success,failure)  
+  }
 
-	render(){
-		return(
-		<div  className = 'description-container'>
-			<div className="header-wrapper">
-                <div className="header">
-                    <div className="logo">
-                        <img src="img/logo2.png" />
-                    </div>
-                </div>
+  handleRate(e, {position, rating, caption, name}) {
+    e.preventDefault()
+    console.log("rated" + rating + " " + position)
+    var el = document.getElementsByClassName('rsr-container')[0]
+    el.style.display = "none"
+    localStorage.hasRated = true
+    var x = document.getElementsByClassName('code-desc-body')[0]
+    var newEl = document.createElement('h3')
+    newEl.style.color = '#db4860'
+    newEl.innerHTML = "Thanks for rating!"
+    x.appendChild(newEl)
+  }
+  render(){
+    var code = this.state.result
+    var lang = _.capitalize(code.language)
+    var diff = _.capitalize(code.difficulty)
+
+    if(localStorage.hasRated)
+      var display = <h3 style={{color: '#db4860'}}> Thanks for rating!</h3>
+    else
+      var display = <StarRating className="star-rating" name="airbnb-rating" size={20}
+             totalStars={5} onRatingClick={this.handleRate.bind(this)} />
+          
+  return(
+      <div  className = 'description-container'>
+        <div className="header-wrapper">
+          <div className="header">
+            <div className="logo">
+              <img src="img/logo2.png" />
             </div>
-         </div>   
-			)
-	}
+            <button className='sign-in'>Login / Register</button>
+          </div>
+        </div>
+        <div className="code-desc-wrapper">
+          <div className="code-desc-header">
+            <h1>{code.name}</h1>
+            <p>{code.description}</p>
+          </div>
+          <div className="code-desc-body">
+            <h2> Difficulty </h2>
+            <h2> Language </h2>
+            <h2> Rating <small>(4.6/5)</small> </h2>
+            <h3> {diff} </h3>
+            <h3>{lang}</h3>
+            {display}
+          </div>
+        </div>
+      </div>   
+    )
+  }
 }
 
 module.exports = CodeDesc;
--- app/components/CodeResultBox.jsx
+++ app/components/CodeResultBox.jsx
@@ -18,11 +18,17 @@ class CodeResultBox extends React.Component{
 		var language = code.language
 		var diff = code.difficulty
 	
+    if(language == "c#")
+      var classVar = "csharp"
+    else if(language == "c++")
+      var classVar = "cpp"
+    else
+      var classVar = language
 
 		return(
 			<div key={code.id} className="code-result-box">
 			  <h1>{code.name}</h1>
-			  <h2 className={code.language}>{code.language}</h2>
+			  <h2 className={classVar}>{code.language}</h2>
 			  <h2 className={code.difficulty}>{code.difficulty}</h2>
               <Link to='codedesc' params={{id: code.id}}><button>view source</button></Link>
 			</div>
